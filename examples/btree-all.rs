80cf882e61b1396642853c8c5daf45cf16086090
yuki0920 <ywsep20@gmail.com>
2025-07-25 06:45:18 +0900

    Add comments

--- examples/btree-all.rs
+++ examples/btree-all.rs
@@ -5,7 +5,7 @@ use relly::buffer::{BufferPool, BufferPoolManager};
 use relly::disk::{DiskManager, PageId};
 
 fn main() -> Result<()> {
-    let disk = DiskManager::open("test.btr")?;
+    let disk = DiskManager::open("simple.rly")?;
     let pool = BufferPool::new(10);
     let mut bufmgr = BufferPoolManager::new(disk, pool);
 
